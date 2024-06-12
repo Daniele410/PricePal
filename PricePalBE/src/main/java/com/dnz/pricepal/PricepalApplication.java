35eb6f691929843a2007ff3295e030c0aeb44490
danmirag <daniele.miraglia_ext@sonepar.com>
2024-06-12 10:42:16 +0200

    first commit

---
+++ PricePalBE/src/main/java/com/dnz/pricepal/PricepalApplication.java
@@ -0,0 +1,13 @@
+package com.dnz.pricepal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricepalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PricepalApplication.class, args);
+    }
+
+}
---
+++ PricePalBE/src/test/java/com/dnz/pricepal/PricepalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnz.pricepal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricepalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
